db3ea12f51932fca369f9bc74d024f7bd8763466
Sean Grogan <sean.grogan@gmail.com>
2022-12-27 15:04:11 -0500

    began parallelizing code

--- generalizable_testbox.py
+++ generalizable_testbox.py
@@ -1,14 +1,17 @@
+import concurrent.futures
 import datetime
 import os
 import socket
 import traceback
+
+from tqdm import tqdm
 
 from pars.parfile_reader import many_parfiles_reader, dump_parfile
 from project_archangel import project_archangel
 from utilities.telegram_bot import telegram_bot_send_message
 
 
-def generalizable_test_box(parfile, computer_name, tests_completed_file):
+def generalizable_test_box(parfile, computer_name, tests_completed_file, *args):
     telegram_bot_send_message(
         f"<pre><b>{computer_name}</b></pre>\n"
         f"Starting!\n"
@@ -48,8 +51,27 @@ def cycle_generalizable_test_box(parfiles_folder, tests_completed_file):
         )
 
 
+def cycle_generalizable_test_box_mp(parfiles_folder, tests_completed_file):
+    parfiles = many_parfiles_reader(parfiles_folder)
+    if not os.path.exists(tests_completed_file):
+        dump_parfile(dict(), tests_completed_file)
+    arg_list = [(
+        parfile,
+        socket.gethostname(),
+        tests_completed_file
+    ) for parfile in tqdm(parfiles)]
+    with concurrent.futures.ProcessPoolExecutor() as PPE:
+        r = PPE.map(generalizable_test_box_wrapper, arg_list)
+        for _ in tqdm(r):
+            pass
+
+
+def generalizable_test_box_wrapper(args):
+    generalizable_test_box(*args)
+
+
 if __name__ == '__main__':
-    cycle_generalizable_test_box(
+    cycle_generalizable_test_box_mp(
         parfiles_folder="./pars/testing_folder_experiments_1/",
         tests_completed_file="./datafiles/tests_completed.json"
     )
