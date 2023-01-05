6597585695300f287233508fdff1de1197554dda
sean grogan <sean.grogan@gmail.com>
2023-01-05 08:54:52 -0500

    Added some helpers here

--- join_datafiles.py
+++ join_datafiles.py
@@ -1,7 +1,9 @@
 from pars.parfile_reader import dump_parfile
 from project_archangel import read_tests_completed_files
+from utilities.file_movers import open_folder
 
 tests_completed_folder = "./datafiles/"
 tests_completed_file = "./datafiles/tests_completed.json"
 tests_completed = read_tests_completed_files(tests_completed_folder)
 dump_parfile(tests_completed, tests_completed_file)
+open_folder(tests_completed_folder)
